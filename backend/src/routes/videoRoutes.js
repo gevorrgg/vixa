b1dad094afd695adbfb6d3803db08251f23233be
Gevorg <gevorg@MacBook-Air-Gevorg.local>
2026-07-08 20:42:16 +0400

    improving ui

--- backend/src/routes/videoRoutes.js
+++ backend/src/routes/videoRoutes.js
@@ -1,16 +1,16 @@
 const express = require('express')
 const VideoController = require('../controllers/videos')
-const { authMiddlewear } = require('../middleware/middleware')
+const { authMiddleware } = require('../middleware/middleware')
 
 const router = express.Router()
 
 router.get('/:userId/videos', VideoController.getUserVideos)
 router.post(
     '/:userId/videos/upload-url',
-    authMiddlewear,
+    authMiddleware,
     VideoController.getVideoUploadUrl
 )
-router.post('/:userId/videos', authMiddlewear, VideoController.createVideo)
-router.delete('/:userId/videos/:videoId', authMiddlewear, VideoController.deleteVideo)
+router.post('/:userId/videos', authMiddleware, VideoController.createVideo)
+router.delete('/:userId/videos/:videoId', authMiddleware, VideoController.deleteVideo)
 
 module.exports = router
