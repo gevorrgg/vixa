1693918a148032b08e0e563b60c52c434951db0d
Gevorg <gevorg@MacBook-Air-Gevorg.local>
2026-07-09 16:18:31 +0400

    improving ui

--- docker/init.sql
+++ docker/init.sql
@@ -61,8 +61,8 @@ CREATE TABLE videos (
 );
 
 CREATE TABLE likes (
-    user_id INT NOT NULL REFERENCES users(id),
-    video_id INT NOT NULL REFERENCES videos(id),
+    user_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    video_id INT NOT NULL REFERENCES videos(id) ON DELETE CASCADE,
     PRIMARY KEY (user_id, video_id)
 );
 
