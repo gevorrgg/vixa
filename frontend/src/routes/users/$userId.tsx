0fc9eaa09c86f1398c911b92c0652799db77e03b
Gevorg <gevorg@MacBook-Air-Gevorg.local>
2026-07-09 21:34:54 +0400

    improving ui

--- frontend/src/routes/users/$userId.tsx
+++ frontend/src/routes/users/$userId.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute("/users/:userId")({
+export const Route = createFileRoute("/users/$userId")({
     component: UserProfilePage,
 });
 
