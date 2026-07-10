8873be1fdbe6a4876d5ac881c78b7b5f6fc2d728
Gevorg <gevorg@MacBook-Air-Gevorg.local>
2026-07-11 03:22:10 +0400

    spliting into microserveces

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -19,8 +19,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  build: {
-    outDir: "../backend/services/api-gateway/src",
-    emptyOutDir: false,
-  },
 });
