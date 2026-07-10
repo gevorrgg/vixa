435d3bf56b31097c14e1c20b5f55a01891d42922
Gevorg <gevorg@MacBook-Air-Gevorg.local>
2026-07-11 03:23:40 +0400

    spliting into microserveces

--- backend/services/api-gateway/src/app.js
+++ backend/services/api-gateway/src/app.js
@@ -1,8 +1,8 @@
-const express = require("express")
-const { createProxyMiddleware } = require("http-proxy-middleware")
+const express = require("express");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 const path = require("path");
 
-const app = express()
+const app = express();
 
 app.use(
     "/api/users",
@@ -10,7 +10,7 @@ app.use(
         target: process.env.USER_SERVICE_URL,
         changeOrigin: true,
     }),
-)
+);
 
 app.use(
     "/api/videos",
@@ -18,15 +18,18 @@ app.use(
         target: process.env.VIDEO_SERVICE_URL,
         changeOrigin: true,
     }),
-)
+);
 
 app.get("/health", (req, res) => {
     res.json({ ok: true });
 });
 
+const distPath = path.join(__dirname, "../dist");
+
+app.use(express.static(distPath));
+
 app.use((req, res) => {
-    res.sendFile(path.join(__dirname, "../dist/index.html"));
-})
+    res.sendFile(path.join(distPath, "index.html"));
+});
 
-
-module.exports = app
+module.exports = app;
