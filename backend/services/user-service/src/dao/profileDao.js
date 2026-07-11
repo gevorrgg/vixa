aad7b4a5bdb56fc6adfcb833418d97058ad5853e
Gevorg <gevorg@MacBook-Air-Gevorg.local>
2026-07-11 08:45:49 +0400

    Adding redis

--- backend/services/user-service/src/dao/profileDao.js
+++ backend/services/user-service/src/dao/profileDao.js
@@ -1,46 +1,54 @@
-const QueryBuilder = require('../db/queryBuilder')
-const db = require('../db/db')
-const { redisClient } = require('../redis/redis')
+const QueryBuilder = require("../db/queryBuilder")
+const db = require("../db/db")
+const { redisClient } = require("../redis/redis")
 
 const fieldMap = {
-    userId: 'user_id',
-    username: 'username',
-    avatarKey: 'avatar_key',
-    name: 'name',
-    location: 'location',
-    bio: 'bio',
-    website: 'website',
-    gender: 'gender'
+    userId: "user_id",
+    username: "username",
+    avatarKey: "avatar_key",
+    name: "name",
+    location: "location",
+    bio: "bio",
+    website: "website",
+    gender: "gender",
 }
 
 const queryBuilder = new QueryBuilder(fieldMap)
 
 class ProfileDao {
     static async create (profile) {
-        const sql = queryBuilder.insert('profiles', profile)
+        const sql = queryBuilder.insert("profiles", profile)
         const params = Object.entries(profile).map(([_, value]) => value)
 
         await db.query(sql, params)
 
-        await redisClient.del(`user:${profile.userId}:hasProfile`)
+        await Promise.all([
+            redisClient.del(`user:${profile.userId}:hasProfile`),
+            redisClient.del(`del:${profile.userId}:userInfo`),
+        ])
     }
 
     static async update (profile, userId) {
-        const sql = queryBuilder.update('profiles', userId, profile)
+        const sql = queryBuilder.update("profiles", userId, profile)
 
         const values = Object.entries(profile).map(element => element[1])
         values.push(userId)
 
         await db.query(sql, values)
+
+        await Promise.all([
+            redisClient.del(`user:${profile.userId}:hasProfile`),
+            redisClient.del(`del:${profile.userId}:userInfo`),
+        ])
     }
 
-    static async hasUserProfile(userId) {
+    static async hasUserProfile (userId) {
         const cacheKey = `user:${userId}:hasProfile`
 
         const cached = await redisClient.get(cacheKey)
 
-        if (cached !== null) { 
-            return cached
+        if (cached !== null) {
+            return cached === "true"
         }
 
         const result = await db.query(
@@ -49,32 +57,32 @@ class ProfileDao {
             FROM profiles
             WHERE user_id = $1
         )`,
-            [userId]
+            [userId],
         )
 
         const hasProfile = result.rows[0].exists
 
         await redisClient.set(cacheKey, String(hasProfile), {
-            EX: 300
+            EX: 300,
         })
 
         return hasProfile
     }
 
-    static async getAvatarKey(userId) {
+    static async getAvatarKey (userId) {
         const cacheKey = `user:${userId}:avatarKey`
 
         const cached = await redisClient.get(cacheKey)
 
-        if (cached !== null) { 
+        if (cached !== null) {
             return cached
         }
-    
+
         const result = await db.query(
             `SELECT avatar_key
             FROM profiles
             WHERE user_id = $1`,
-            [userId]
+            [userId],
         )
 
         const avatarKey = result.rows[0]?.avatar_key || null
@@ -84,23 +92,22 @@ class ProfileDao {
         }
 
         await redisClient.set(cacheKey, avatarKey, {
-            EX: 300
+            EX: 300,
         })
 
         return avatarKey
     }
 
-    static async updateAvatarKey(userId, avatarKey) {
-        const sql = queryBuilder.update('profiles', userId, { avatarKey })
+    static async updateAvatarKey (userId, avatarKey) {
+        const sql = queryBuilder.update("profiles", userId, { avatarKey })
         const values = [avatarKey, userId]
 
         await db.query(sql, values)
 
         const cacheKey = `user:${userId}:avatarKey`
         await redisClient.set(cacheKey, avatarKey, {
-            EX: 300
+            EX: 300,
         })
-
     }
 }
 
